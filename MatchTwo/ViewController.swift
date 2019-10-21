427a38452114273d2c65b0d0e8fd29061ddc9fef
olegpon <oleg.ponomarev@rasolution.ru>
2019-10-21 19:31:26 +0500

    Init

--- MatchTwo/ViewController.swift
+++ MatchTwo/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-
+    var emojiChoices = ["🐨", "🌝", "🐨", "🌝",]
+    
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardNumber = cardButtons.firstIndex(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
+        
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        flipCount += 1
+        if button.currentTitle == emoji{
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        }else{
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
